6a86f7f5f55e50eeeef6faf6d9fcaa1d035003f3
mahaamimran <mahaamimran@gmail.com>
2023-08-15 13:52:17 +0500

    list and gallery view outline

--- lib/home.dart
+++ lib/home.dart
@@ -6,10 +6,8 @@ class Home extends StatefulWidget {
   @override
   _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
-  bool isGalleryView = false; // Default to list view
-
+  bool isGalleryView = false;
   double containerWidth = 100;
   double containerHeight = 100;
 
@@ -21,122 +19,54 @@ class _HomeState extends State<Home> {
         padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.04),
         children: [
           if (isGalleryView)
-            Column(
-              children: [
-                // row 1
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        // Handle the click for the first container
-                        print('tapped box 1');
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: MediaQuery.of(context).size.width * 0.4,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(120, 142, 141, 0.56),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        // Handle the click for the second container
-                        print('tapped box 2');
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: MediaQuery.of(context).size.width * 0.4,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(120, 142, 141, 0.56),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                // space between two rows
-                SizedBox(
-                  height: MediaQuery.of(context).size.width * 0.04,
-                ),
-                // row 2
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                  children: [
-                    GestureDetector(
-                      onTap: () {
-                        // Handle the click for the first container
-                        print('tapped box 1');
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: MediaQuery.of(context).size.width * 0.4,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(120, 142, 141, 0.56),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                    GestureDetector(
-                      onTap: () {
-                        // Handle the click for the second container
-                        print('tapped box 2');
-                      },
-                      child: Container(
-                        width: MediaQuery.of(context).size.width * 0.4,
-                        height: MediaQuery.of(context).size.width * 0.4,
-                        decoration: BoxDecoration(
-                          color: Color.fromRGBO(120, 142, 141, 0.56),
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-              ],
+            Wrap(
+              spacing: 10.0,
+              runSpacing: 10.0,
+              alignment: WrapAlignment.center,
+              children: _buildContainers(),
             )
           else
             Column(
               mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-              children: [
-                Container(
-                  height: 100,
-                  width: 300,
-                  color: Colors.blue,
-                ),
-                SizedBox(height: 10),
-                Container(
-                  height: 100,
-                  width: 300,
-                  color: Colors.blue,
-                ),
-                SizedBox(height: 10),
-                Container(
-                  height: 100,
-                  width: 300,
-                  color: Colors.blue,
-                ),
-              ],
+              children: _buildContainers(),
             ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
           setState(() {
-            isGalleryView =
-                !isGalleryView; // Toggle between list and gallery view
+            isGalleryView = !isGalleryView;
             if (isGalleryView) {
-              containerWidth = 150; // Update width for gallery view
-              containerHeight = 75; // Update height for gallery view
+              containerWidth = MediaQuery.of(context).size.width * 0.4;
+              containerHeight = MediaQuery.of(context).size.width * 0.4;
             } else {
-              containerWidth = 100; // Reset width for list view
-              containerHeight = 100; // Reset height for list view
+             containerWidth = MediaQuery.of(context).size.width * 0.9;
+              containerHeight = MediaQuery.of(context).size.width * 0.3;
             }
           });
         },
         child: Icon(isGalleryView ? Icons.view_list : Icons.grid_view),
+      ),
+    );
+  }
+
+  List<Widget> _buildContainers() {
+    return List.generate(
+      22,
+      (index) => GestureDetector(
+        onTap: () {
+          // Handle the click for the container
+          print('tapped box $index');
+        },
+        child: Container(
+          width: containerWidth,
+          height: containerHeight,
+           margin: EdgeInsets.only(bottom: MediaQuery.of(context).size.width * 0.04),
+          decoration: BoxDecoration(
+            color: Color.fromRGBO(120, 142, 141, 0.56),
+            borderRadius: BorderRadius.circular(20),
+          ),
+        ),
       ),
     );
   }
