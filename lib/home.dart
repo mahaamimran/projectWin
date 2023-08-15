4041e1331cb2b18f0a35569f6cc4c059b0a3750d
mahaamimran <mahaamimran@gmail.com>
2023-08-15 12:41:59 +0500

    gallery

--- lib/home.dart
+++ lib/home.dart
@@ -1,15 +1,110 @@
 import 'package:flutter/material.dart';
 
+
 class Home extends StatefulWidget {
   const Home({super.key});
 
   @override
-  State<Home> createState() => _HomeState();
+  _HomeState createState() => _HomeState();
 }
-
 class _HomeState extends State<Home> {
+  bool isGalleryView = false; // Default to list view
+
+  double containerWidth = 100;
+  double containerHeight = 100;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(255, 255, 255, 255),
+      body: ListView(
+        padding: EdgeInsets.all(MediaQuery.of(context).size.width * 0.04),
+        children: [
+          if (isGalleryView)
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.blue,
+                    borderRadius: BorderRadius.circular(20),
+                  ),
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.green,
+                    shape: BoxShape.circle,
+                  ),
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.orange,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.only(
+                      topLeft: Radius.circular(20),
+                      bottomRight: Radius.circular(20),
+                    ),
+                  ),
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  decoration: BoxDecoration(
+                    color: Colors.purple,
+                    shape: BoxShape.rectangle,
+                    borderRadius: BorderRadius.circular(50),
+                  ),
+                ),
+              ],
+            )
+          else
+            Column(
+              children: [
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  color: Colors.red,
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  color: Colors.green,
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  color: Colors.blue,
+                ),
+                Container(
+                  width: containerWidth,
+                  height: containerHeight,
+                  color: Colors.yellow,
+                ),
+              ],
+            ),
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          setState(() {
+            isGalleryView = !isGalleryView; // Toggle between list and gallery view
+            if (isGalleryView) {
+              containerWidth = 150; // Update width for gallery view
+              containerHeight = 75; // Update height for gallery view
+            } else {
+              containerWidth = 100; // Reset width for list view
+              containerHeight = 100; // Reset height for list view
+            }
+          });
+        },
+        child: Icon(isGalleryView ? Icons.view_list : Icons.grid_view),
+      ),
+    );
   }
 }
--- lib/mainPage.dart
+++ lib/mainPage.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: file_names
+
 import 'package:flutter/material.dart';
 
 import 'home.dart';
@@ -37,45 +39,51 @@ class _MyAppState extends State<MyApp> {
                 fontSize: MediaQuery.of(context).size.width * 0.055,
               ),
             ),
-            backgroundColor: const Color(0xff212327),
+            backgroundColor: const Color(0xFF677C7B),
           ),
           body: _pages[_selectedIndex],
-          bottomNavigationBar: Container(
-            child: Padding(
-              padding: const EdgeInsets.only(
-                bottom: 35,
-                left: 40,
-                right: 40,
-              ),
-              child: ClipRRect(
-                borderRadius: BorderRadius.circular(50),
+          /*
+          bottomNavigationBar: Padding(
+            padding: const EdgeInsets.only(
+              bottom: 35,
+              left: 40,
+              right: 40,
+            ),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(50),
+              child: Expanded(
                 child: BottomNavigationBar(
-
-                  iconSize: 40,
-                  elevation: 10, 
                   showSelectedLabels: false,
                   showUnselectedLabels: false,
-                  backgroundColor: Color.fromARGB(
-                      190, 217, 217, 217), 
+                  backgroundColor: const Color(0xD8D9D9D9),
+                  unselectedItemColor: const Color.fromARGB(150, 120, 142, 141),
                   selectedItemColor: const Color.fromARGB(255, 55, 73, 87),
-                  unselectedItemColor: const Color.fromARGB(200, 55, 73, 87),
                   currentIndex: _selectedIndex,
                   onTap: (index) {
                     setState(() {
                       _selectedIndex = index;
                     });
                   },
-                  items: const [
+                  items: [
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.home),
+                      icon: Image.asset(
+                        'assets/navigationbar/home.png',
+                        scale: 0.8,
+                      ),
                       label: '',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.favorite),
+                      icon: Image.asset(
+                        'assets/navigationbar/favourites.png',
+                        scale: 0.8,
+                      ),
                       label: '',
                     ),
                     BottomNavigationBarItem(
-                      icon: Icon(Icons.settings),
+                      icon: Image.asset(
+                        'assets/navigationbar/settings.png',
+                        scale: 0.8,
+                      ),
                       label: '',
                     ),
                   ],
@@ -83,6 +91,7 @@ class _MyAppState extends State<MyApp> {
               ),
             ),
           ),
+          */
         );
       }),
       debugShowCheckedModeBanner: false,
